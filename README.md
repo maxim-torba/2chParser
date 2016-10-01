57e00c9981a3b8c1771f7b5d4c5c70596d1b1b93
maxim <maxim.torba@gmail.com>
2016-10-02 01:52:00 +0300

    add feature for wait new posts and download them files add progress bar

--- README.md
+++ README.md
@@ -3,12 +3,14 @@ It's a parcer for 2ch.hk
 
 This is desktop application working on JavaScript built on [ElectronJS](http://electron.atom.io/)
 
-<img src='/screenshots/Screenshot_1.png' width='300px'>
-<img src='/screenshots/Screenshot_2.png' width='300px'>
+<img src='/screenshots/Screenshot_1.png' width='500px'>
+<img src='/screenshots/Screenshot_2.png' width='500px'>
+<img src='/screenshots/Screenshot_3.png' width='500px'>
+
 
 You need to setup the [NodeJS](https://nodejs.org/en/) for the following actions.
 
-For install the project you should clone or download this repository and then run command **npm install**. 
+For install the project you should clone or download this repository and then run command **npm install**.
 
-For get working program (while only for windows) you should run command **npm run dist**, and after you will see folder 
+For get working program (while only for windows) you should run command **npm run dist**, and after you will see folder
 **dist** (in project directory) where you find packed (the installation file) and the unpacked program ready to work.
--- parser.js
+++ parser.js
@@ -4,6 +4,7 @@ const url = require('url');
 const fs = require('fs');
 
 const request = require('request');
+const progress = require('request-progress');
 
 const electron = require('electron');
 const remote = electron.remote;
@@ -13,13 +14,20 @@ var withImgs = false;
 var fileCounter = 0;
 var arrOfLinks = [];
 var addressLink = '';
+var isWatching = false;
+var isBreak = false;
+var newPostsTimer;
 
 var folder = __dirname + "\\data\\";
 
 $('#folderAddress').val(folder);
 
-exports.getFiles = function (address) {
+exports.getFiles = getFiles;
+
+function getFiles(address) {
     addressLink = address;
+    arrOfLinks = [];
+    isBreak = false;
     address = address.replace('.html', '.json');
     request({uri: address, method: 'GET', encoding: 'utf8'},
         (err, res) => {
@@ -28,25 +36,25 @@ exports.getFiles = function (address) {
                 return;
             }
             //console.log(res.body);
-            withImgs = $('#withImages').prop("checked");
+            withImgs = $('#withImages').find('input').prop("checked");
             
             try {
                 JSON.parse(res.body).threads[0].posts.forEach((post) => {
                     post.files.forEach((file) => {
-                        //  downloadFile(file.path); 2ch ban me when i try download all files at the same time
                         arrOfLinks.push(file.path);
                     });
                 });
-                showAmount();
+                showProgress();
                 downloadFile(arrOfLinks[fileCounter]);
             }
             catch (e) {
                 console.error(e);
-                var is404 = /<span class="notfound404">404<\/span>/.test(res.body);
+                let is404 = /<span class="notfound404">404<\/span>/.test(res.body);
                 if (is404)
                     alert('Page not found, error 404');
                 else
                     alert('Can\'t parse this page');
+                clearInterval(newPostsTimer);
                 return;
             }
             
@@ -54,42 +62,60 @@ exports.getFiles = function (address) {
                 $('#btn-break').fadeIn(600);
             });
         });
-};
-
-function downloadFile(path) {
-    var ulrObj = url.parse(addressLink);
-    var protocol = ulrObj.protocol;
-    var host = ulrObj.host;
-    var board = /\/\w{0,5}\//.exec(ulrObj.pathname);
-    
-    var href = protocol + "//" + host + board + path;
-    var name = /\d{13,15}/.exec(path);
-    var extension = /\.\w{0,10}/.exec(path);
-    var fileAddress = folder + name + extension;
-    console.log(href);
-    
-    let file = fs.createWriteStream(fileAddress);
-    console.log(folder + name + extension);
-    request(href)
-        .on('end', () => {
-            if (arrOfLinks.length && fileCounter < arrOfLinks.length) {
-                downloadFile(arrOfLinks[fileCounter]);
-                fileCounter++;
-                updateStatus();
-            }
-            else {
-                alert('thread was successfully parsed');
-                toBegin();
-            }
-        })
-        .on('error', (err) => {
-            console.error(err);
-        })
-        .pipe(file);
 }
 
-function showAmount() {
+function downloadFile(path) {
+    let ulrObj = url.parse(addressLink);
+    let protocol = ulrObj.protocol;
+    let host = ulrObj.host;
+    let board = /\/\w{0,5}\//.exec(ulrObj.pathname);
+    
+    let href = protocol + "//" + host + board + path;
+    let name = /\d{13,15}/.exec(path);
+    let extension = /\.\w{0,10}/.exec(path);
+    let fileAddress = folder + name + extension;
+    
+    console.log(href);
+    
+    if (extension != '.webm' && !withImgs) {
+        counterHandler();
+        return;
+    }
+    
+    if (typeof path != 'undefined') {
+        let file = fs.createWriteStream(fileAddress);
+        
+        progress(request(href).on('end', counterHandler), {
+            throttle: 0
+        }).on('progress', function (state) {
+            $('#progress-bar').css('width', state.percentage * 100 + '%');
+        }).on('error', function (err) {
+            console.error(err);
+        }).pipe(file)
+        
+    } else {
+        counterHandler();
+    }
+    
+    function counterHandler() {
+        if (arrOfLinks.length && fileCounter < arrOfLinks.length) {
+            downloadFile(arrOfLinks[++fileCounter]);
+            updateStatus();
+        }
+        else if (!isBreak && !isWatching) {
+            let needWatching = confirm('Thread was successfully parsed. \n' +
+                'Do I need to wait for new posts and download them files?');
+            if (needWatching)
+                newPostsWatcher();
+            else
+                toBegin();
+        }
+    }
+}
+
+function showProgress() {
     $('#amount').fadeIn(600);
+    $('#progress-wrapper').fadeIn(700);
     updateStatus();
 }
 
@@ -97,12 +123,23 @@ function updateStatus() {
     $('#amount').text(fileCounter + ' out of ' + arrOfLinks.length);
 }
 
+function newPostsWatcher() {
+    isWatching = true;
+    newPostsTimer = setInterval(getFiles, 10000, addressLink);
+}
+
 function toBegin() {
+    isBreak = true;
+    
     $('#amount').fadeOut();
+    
+    $('#progress-wrapper').fadeOut();
     
     $('#btn-break').fadeOut(300, () => {
         $('#btn-get').fadeIn(600);
     });
+    isWatching = false;
+    clearInterval(newPostsTimer);
     arrOfLinks.length = 0;
     fileCounter = 0;
 }
